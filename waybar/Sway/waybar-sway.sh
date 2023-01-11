112c8f183c5c2f90d21ee718bbc283e39fb4cb7c
Luiza1011 <LouiseLBlue11@outlook.com>
2023-01-11 14:06:38 -0300

    first commit

---
+++ waybar/Sway/waybar-sway.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+waybar -b 1 -c /home/luiza/.config/waybar/Sway/config_sway_1 -s /home/luiza/.config/waybar/Sway/style_sway_1.css
+waybar -b 2 -c /home/luiza/.config/waybar/Sway/config_sway_2 -s /home/luiza/.config/waybar/Sway/style_sway_2.css
